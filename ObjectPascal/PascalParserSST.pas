d0a45d18b6a6b78db7420bd936d767909e7074ea
Gevorg Nakhapetyan <ngs22071993@gmail.com>
2019-08-08 01:17:18 +0300

    fixed

--- ObjectPascal/PascalParserSST.pas
+++ ObjectPascal/PascalParserSST.pas
@@ -3,7 +3,7 @@
 (*                         The MIT License (MIT)                             *)
 (*                                                                           *)
 (*       Copyright (c) 2019 Nakhapetyan Gevorg <ngs22071993@gmail.com>       *)
-(*         Pascal parser to simple structure text format files (SST)         *)
+(*         Pascal parser of simple structure text format files (SST)         *)
 (*                                                                           *)
 (*===========================================================================*)
 unit PascalParserSST;
